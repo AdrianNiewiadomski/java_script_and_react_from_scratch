126730a2abb0e5c4387d394e7e234722b6d2cd01
AdrianNiewiadomski <kontakt@adrianniewiadomski.pl>
2020-10-31 15:05:04 +0100

    The lesson about variables. WiP.

--- 1_datatypes_and_variables/script.js
+++ 1_datatypes_and_variables/script.js
@@ -1,8 +1,21 @@
-var my_var = 2;
-function ali(myvar) {
-    return myvar*2;
-}
-var aa = 'aa';
-var aa = NaN;
+// var my_var = 2;
+// function ali(myvar) {
+//     return myvar*2;
+// }
+// var aa = 'aa';
+// var aa = true;
 
-console.log(aa);
+// console.log(aa);
+
+// var d = document;
+// console.log(d);
+
+
+var isPasswordCorrect = true;
+console.log(isPasswordCorrect);
+
+var myString = 'Mary had a little lamb';
+console.log(myString);
+
+var myDoc = document;
+console.log(myDoc);      
